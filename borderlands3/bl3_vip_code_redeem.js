9a4a1c91a450b0134e9e587344a6ec549a61812a
rockdevourer <38840840+rockdevourer@users.noreply.github.com>
2019-09-10 22:26:08 -0400

    Update bl3_vip_code_redeem.js
    Complete revamp, does all code types besides Diamond (those are one-time-use as far as I know), and "alerts" results

--- borderlands3/bl3_vip_code_redeem.js
+++ borderlands3/bl3_vip_code_redeem.js
@@ -8,17 +8,32 @@ email type: cid = 5264
 creator code: cid = 5263
 boost type: 5721
 
-After login, navigate to https://2kgames.crowdtwist.com/widgets/t/code-redemption/9902/#2
+After login, navigate to 
+https://2kgames.crowdtwist.com/ -- This is a blank page, as there is no landing page
+https://2kgames.crowdtwist.com/widgets/t/code-redemption/9902/#2 -- This is one of the components of the redemption page, if you'd rather see that
+
+
 
 */
 
+var badcodes = [];
+var pointsredeemed = [];
+var points = 0;
+var temppoints;
+var totalpointsredeemed = 0;
+var emailpointsredeemed = 0;
+var creatorpointsredeemed = 0;
+var vaultpointsredeemed = 0;
+var emailpoints;
+var creatorpoints;
+var vaultpoints;
 
-
-const apibase = 'https://2kgames.crowdtwist.com/code-redemption-campaign/redeem?cid=';
+var apibase = 'https://2kgames.crowdtwist.com/code-redemption-campaign/redeem?cid=';
 
 async function httppost(codeinfo) {
 var response = await fetch(apibase + codeinfo.cid, {
      method: 'POST',
+	 mode: 'cors',
      body: JSON.stringify({
          code: codeinfo.code
      }),
@@ -27,16 +42,27 @@ var response = await fetch(apibase + codeinfo.cid, {
      }
 })
 
-console.log(response);
+let result = await response.json();
+
+if (result.points) {
+	pointsredeemed.push(result.points);
+	temppoints = result.points;
+	return temppoints;
+} else {
+	badcodes.push(codeinfo.code);
+	return null;
+}
+
+
 return null;
 }
-const vaultcid = 5261;
-const emailcid = 5264;
-const creatorcid = 5263;
-const boostcid = 5721;
+var vaultcid = 5261;
+var emailcid = 5264;
+var creatorcid = 5263;
+var boostcid = 5721;
 
 
-const EMAILS = [
+var EMAILS = [
     "BL3REVEAL",
     "BL3WELCOME",
     "LOOTLOOTLOOT",
@@ -58,20 +84,173 @@ const EMAILS = [
     "ABCEASYAS123",
     "DUCTTAPEMOD",
     "MADSKILLZ",
-    "ITSHERE"
+    "ITSHERE",
+	"JABBER",
+	"Unblinkingeye"
+]
+
+var CREATORS = [
+    "PROFESSORBROMANVIP",
+    "KINGGOTHALIONVIP",
+    "PietSmietVIP",
+    "JoltzDude139VIP",
+    "JvtvVIP",
+    "AdmiralbahrooVIP",
+    "Ki11erSixVIP",
+    "MitsuShowVIP",
+    "TessachkaVIP",
+    "LaraLoftVIP",
+    "MitsuShowE3",
+    "TessachkaE3",
+    "EdEMonsterE3",
+    "Joltzdude139E3",
+    "Ki11ersixE3",
+    "KingGothalionE3",
+    "ProfessorBromanE3",
+    "AdmiralBahrooE3",
+    "dammit2hellE3",
+    "NAYSYE3",
+    "LUCKYBONEZE3",
+    "GRIMMBONEZE3",
+    "CohhVIP",
+    "Cure4Kids",
+    "GREGORONKH"
+]
+
+var VAULTS = [
+    "WUBWUBWUB",
+    "JOYPUKE",
+    "LOADINGBAR",
+    "IGNVIP",
+    "Borderlands3vip",
+    "TWITCHVIP",
+    "GAMESPOTVIP",
+    "FACEBOOKVIP",
+    "SHACKNEWSVIP",
+    "GAMEVIP",
+    "WEGOTUBOO",
+    "LOWCOSTCOSPLAYE3",
+    "BORDERLANDSCOSPLAYE3",
+    "ILOVEBAGUETTE",
+    "BL3FREVENTVIP",
+    "WHATCHUWANT",
+    "MUSHYSNUGGLEBITES",
+    "OOPS",
+    "BORDERLENS",
+    "Skagbait",
+    "VIP-GUNWALL",
+    "VIP-GIFTSHOP",
+    "VIP-REDCHEST",
+    "VIP-COV",
+    "VIP-MOXXI",
+    "VIP-ENVART",
+    "VIP-CALYPSO",
+    "VIP-CHARCONCEPT",
+    "VIP-BOXCONCEPT",
+    "VIP-PHOTOBOOTH",
+    "BL3MUSEUMCOSPLAY",
+    "3DJUEGOSVIP",
+    "HALVERHAHN",
+    "BRINGMEABUCKET",
+    "MAYHEMPRIDE",
+    "PWR2PLYRS",
+    "THESEPRETZELSSUCK",
+    "TINKABOUTIT",
+    "LOOTSPLOSION",
+    "BONERFART",
+    "LETSMAKESOMEMAYHEM",
+    "MAYHEM",
+    "CRUMPOCALYPSE",
+    "Childrenofthevault",
+    "WACHSK4G",
+    "JABBERKUMP3L",
+    "D1G1KLON",
+    "RA1LGUN",
+    "5piderantcenturio",
+    "Phasenkl4mmerung",
+    "1ronbear",
+    "Sntnl",
+    "Phasenproj3ktion",
+    "Ph4senschlag",
+    "1CODE",
+    "GODSDONTNEGOTIATE",
+    "Ehrenmann",
+    "TWOWEEKS",
+    "VIP-PAXINIT",
+    "VIP-PAXALAX",
+    "VIP-SUPERPAX",
+    "VIP-EATINGPAX",
+    "JVMVIP",
+    "VIP-LOOTPAX",
+    "VIP-PAXSCAVENGER",
+    "PAXSCAVENGER",
+    "VIP-PAXATYA",
+    "VIP-PAXATTACK",
+    "VIP-TAKEITTOTHEPAX",
+    "VIP-PAXTASTIC",
+    "SMARTOYSVIP",
+    "MEDIAMARKTVIP",
+    "Bolvip",
+    "GAMEMANIAVIP",
+    "ALLYOURGAMESVIP",
+    "NEDGAMEVIP",
+    "YOURGAMEZONEVIP",
+    "VIP-DEATHANDPAXES",
+    "VIP-SMACKTHATPAX",
+    "VIP-KEEPINGITPAX",
+    "INTERTOYSVIP",
+    "PLAYERONEVIP",
+    "VIP-ANOTHERPAXPUN",
+    "vip-paxagainstthewall",
+    "SEVENDAYS",
+    "AIRLEMAGVIP",
+	"Dreamlandvip"
 ]
 
 
-async function main() {
-	EMAILS.forEach(async function(email) {
+async function emailmap() {
+	emailpoints = await Promise.all(EMAILS.map(async function(email) {
 		var type = {
-		code : email,
-		cid : emailcid
+			code : email,
+			cid : emailcid
 		};
-		//console.log(type);
-		httppost(type);
-		//test = await console.log(email);
-	})
+		temppoints = await httppost(type);
+		emailpointsredeemed = emailpointsredeemed + temppoints;
+		return emailpointsredeemed;
+}))};
+
+async function creatorsmap() {
+	creatorspoints = await Promise.all(CREATORS.map(async function(creator) {
+	var type = {
+		code : creator,
+		cid : creatorcid
+	};
+	temppoints = await httppost(type);
+	creatorpointsredeemed = creatorpointsredeemed + temppoints;
+	return creatorpointsredeemed;
+}))};
+
+async function vaultmap() {
+	vaultpoints = await Promise.all(VAULTS.map(async function(vault) {
+	var type = {
+		code : vault,
+		cid : vaultcid
+	};
+	temppoints = await httppost(type);
+	vaultpointsredeemed = vaultpointsredeemed + temppoints;
+	return vaultpointsredeemed;
+}))}
+
+async function gatherpoints() {
+	var [totalemailpoints, totalcreatorpoints, totalvaultpoints] = await Promise.all([emailmap(), creatorsmap(), vaultmap()]);
+	var totalpoints = emailpointsredeemed + creatorpointsredeemed + vaultpointsredeemed;
+	console.log("Total Points: ", totalpoints);
+	alert("Total Points Redeemed: " + totalpoints)
+}
+
+
+async function main() {
+	gatherpoints();
 }
 
 main();
